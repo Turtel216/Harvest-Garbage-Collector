82dcf8d8033699f5ed1d04a382f046418274ef10
Turtel216 <papadimitrios216@gmail.com>
2024-08-01 01:15:06 +0300

    Added performance test

---
+++ test/perf_test.c
@@ -0,0 +1,17 @@
+#include "../src/garbage_collector.h"
+
+int main(void)
+{
+	struct VM *vm = newVM();
+
+	for (int i = 0; i < 1000; i++) {
+		for (int j = 0; j < 20; j++) {
+			pushInt(vm, i);
+		}
+
+		for (int k = 0; k < 20; k++) {
+			pop(vm);
+		}
+	}
+	free_vm(vm);
+}
